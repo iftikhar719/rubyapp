11b9cd661c4f0942f6f90c91b9e39156b824814d
iftikhar ahmed <iftikhar.ah719@gmail.com>
2021-02-18 19:05:57 +0500

    first push

--- app/controllers/application_controller.rb
+++
@@ -1,9 +0,0 @@
-class ApplicationController < ActionController::Base
-    def hello
-    render html: "goodbye, world!"
-    end
-
-    def goodbye
-      render html: "goodbye, world!"
-    end
-end
